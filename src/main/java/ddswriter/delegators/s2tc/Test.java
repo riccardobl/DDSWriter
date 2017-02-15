dd134cfe20b3099bef57d2f858631bcb2d41bd5c
Riccardo Balbo <riccardo@forkforge.net>
2017-02-15 16:38:12 +0100

    Add quick test for s2tc compression

---
+++ src/main/java/ddswriter/delegators/s2tc/Test.java
@@ -0,0 +1,10 @@
+package ddswriter.delegators.s2tc;
+
+import ddswriter.cli.Main;
+
+public class Test{
+	public static void main(String[] _args) throws Exception {
+		String args[]="--compress --in /tmp/tobecompressed.jpg --out /tmp/tx.dds".split(" ");
+		Main.main(args);
+	}
+}
